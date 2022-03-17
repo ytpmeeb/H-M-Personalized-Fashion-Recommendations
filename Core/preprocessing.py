deb8a32f5136445a1f6fc9553decda23f605e839
Shengkai <85941465+ytpmeeb@users.noreply.github.com>
2022-03-17 14:58:23 -0400

    Update preprocessing.py

--- Core/preprocessing.py
+++ Core/preprocessing.py
@@ -5,30 +5,96 @@ from datetime import *
 # reduce memory by transforming customer_id from 64 bytes (String) to 8 bytes (Int)
 # https://www.kaggle.com/c/h-and-m-personalized-fashion-recommendations/discussion/308635
 def reduceMemory(name, df):
-    df[name] = train[name].apply(lambda x: int(x[-16:], 16)).astype('int64')
+    df[name] = df[name].apply(lambda x: int(x[-16:], 16)).astype('int64')
     return df
 
 
-# Data Reduction on customer.csv
-customer_csv = 'C:/Users/naerkond/Programming_Languages/Projects_Python/Playground/customer.csv'
-train = pd.read_csv(customer_csv)
-reduceMemory('customer_id', train)
-train.to_csv('customer_new.csv', sep=',', encodings='UTF-8', index=None, header=True)
+def customer_data(data: []):
+    reduceMemory('customer_id', data)
+    reduceMemory('postal_code', data)
+
+    # find the unique value
+    # pd.unique(test['club_member_status'])
+
+    # check and delete duplication
+    if data.duplicated(subset=['customer_id']).sum() > 0:
+        data.drop_duplicates(subset=['customer_id'])
+
+    # check and delete missing value
+    data = data.drop('FN', 1)
+    data = data.drop('Active', 1)
+    data = data.dropna(subset=['age', 'club_member_status', 'fashion_news_frequency'])
+
+    # change club_member_status to numeric: ACTIVE=2, PRE-CREATE=1, LEFT CLUB=0
+    data.loc[data['club_member_status'] == 'ACTIVE', 'club_member_status'] = 2
+    data.loc[data['club_member_status'] == 'PRE-CREATE', 'club_member_status'] = 1
+    data.loc[data['club_member_status'] == 'LEFT CLUB', 'club_member_status'] = 0
+
+    # change fashion_news_frequency to numeric: Monthly=2, Regularly=1, NONE(None)=0
+    data.loc[data['fashion_news_frequency'] == 'Monthly', 'fashion_news_frequency'] = 2
+    data.loc[data['fashion_news_frequency'] == 'Regularly', 'fashion_news_frequency'] = 1
+    data.loc[data['fashion_news_frequency'] == 'NONE', 'fashion_news_frequency'] = 0
+    data.loc[data['fashion_news_frequency'] == 'None', 'fashion_news_frequency'] = 0
+
+    data.to_csv('dataing_customer.csv', sep=',', encoding='UTF-8', index=None, header=True)
 
 
-# Data Reduction on transaction.csv
-tx_csv = 'C:/Users/naerkond/Programming_Languages/Projects_Python/Playground/transactions_train.csv'
-train = pd.read_csv(tx_csv)
-train['t_dat'] = pd.to_datetime(train['t_dat'])
-train['t_dat'] = train['t_dat'].dt.date
-reduceMemory('customer_id', train)
+def transaction_data(data: []):
+    data['t_dat'] = pd.to_datetime(data['t_dat'])
+    data['t_dat'] = data['t_dat'].dt.date
+    reduceMemory('customer_id', data)
 
-# split transaction into years and generate csv
-tx2018 = train.loc[train['t_dat'] <= date(2019, 9, 20)]
-tx2019 = train.loc[(date(2019, 9, 20) < train['t_dat']) & (train['t_dat'] <= date(2020, 9, 20))]
-tx2020 = train.loc[(date(2020, 9, 20) < train['t_dat']) & (train['t_dat'] <= date(2021, 9, 20))]
-tx2018.to_csv('transaction_2018.csv', sep=',', encoding='UTF-8', index=None, header=True)
-tx2019.to_csv('transaction_2019.csv', sep=',', encoding='UTF-8', index=None, header=True)
-tx2020.to_csv('transaction_2020.csv', sep=',', encoding='UTF-8', index=None, header=True)
+    # delete price and sales_channel_id value
+    data = data.drop('price', 1)
+    data = data.drop('sales_channel_id', 1)
+
+    # find customer who purchase more than 1 times
+    a = pd.DataFrame(data.loc[data['t_dat'] == date(2020, 9, 21), 'customer_id'])
+    a = a.drop_duplicates()
+    b = data.loc[data['t_dat'] == date(2020, 9, 21)]
+    b = pd.DataFrame(b.loc[b.customer_id.isin(a.customer_id), 'customer_id'])
+    b = b.drop_duplicates()
+    # split transaction into years and generate csv
+    tx2018 = data.loc[data['t_dat'] <= date(2019, 9, 20)]
+    tx2019 = data.loc[(date(2019, 9, 20) < data['t_dat']) & (data['t_dat'] <= date(2020, 9, 20))]
+    tx2020 = data.loc[(date(2020, 9, 20) < data['t_dat']) & (data['t_dat'] <= date(2021, 9, 20))]
+    tx2018.to_csv('transaction_2018.csv', sep=',', encoding='UTF-8', index=None, header=True)
+    tx2019.to_csv('transaction_2019.csv', sep=',', encoding='UTF-8', index=None, header=True)
+    tx2020.to_csv('transaction_2020.csv', sep=',', encoding='UTF-8', index=None, header=True)
 
 
+def article_data(data: []):
+    # delete nonnumerical  value
+    data = data.drop('prod_name', 1)
+    data = data.drop('product_type_name', 1)
+    data = data.drop('product_group_name', 1)
+    data = data.drop('graphical_appearance_name', 1)
+    data = data.drop('colour_group_name', 1)
+    data = data.drop('perceived_colour_value_name', 1)
+    data = data.drop('perceived_colour_master_name', 1)
+    data = data.drop('department_name', 1)
+    data = data.drop('index_code', 1)
+    data = data.drop('index_name', 1)
+    data = data.drop('index_group_name', 1)
+    data = data.drop('section_name', 1)
+    data = data.drop('garment_group_name', 1)
+    data = data.drop('detail_desc', 1)
+
+    # check and delete missing value
+    data = data.dropna()
+
+    data.to_csv('training_article.csv', sep=',', encoding='UTF-8', index=None, header=True)
+
+
+# data1 = pd.read_csv('/Users/nakreond/Programming_Languages/Python_Projects/H&M Personalized Fashion Recommendations/customers.csv')
+data1 = pd.read_csv('C:/Users/naerkond/Programming_Languages/GitHub/HnM_Personalized_Fashion_Recommendations/Datasets/customers.csv')
+
+# data2 = pd.read_csv('/Users/nakreond/Programming_Languages/Python_Projects/H&M Personalized Fashion Recommendations/H_M_transaction_2020.csv')
+# data2 = pd.read_csv('C:/Users/naerkond/Programming_Languages/GitHub/HnM_Personalized_Fashion_Recommendations/Datasets/transactions_train.csv')
+
+# data3 = pd.read_csv('/Users/nakreond/Programming_Languages/Python_Projects/H&M Personalized Fashion Recommendations/articles.csv')
+data3 = pd.read_csv('C:/Users/naerkond/Programming_Languages/GitHub/HnM_Personalized_Fashion_Recommendations/Datasets/articles.csv')
+
+customer_data(data1)
+# transaction_data(data2)
+article_data(data3)
--- main.py
+++ main.py
@@ -1,15 +1,40 @@
+"""
+03/16 Meeting
+
+focus on:
+1. project should focus more on Event of item, like what item was bought at what day
+row is time, range can be last 30 days purchase
+col is purchase event, the feature what customer did or product
+
+2. eliminate the one time shopper and focus on more than 2 times purchase customer
+
+3. customer who have more than 2 times purchase vs who didn't purchase
+
+4. what customer do before they purchase
+
+5. use the pattern discover to find out first, than  use supervise classification to find out correct answers
+
+
+training dataset can be separated in tables:
+1. user: profile about each user
+2. purchase with ITEM item and who buy it (time, customerID,  itemID)
+3. what item belong to what category and price
+"""
+
+from Tools import Anomaly_Detection
+from Tools import Data_Integration
+from Tools import Data_Reduction
+from Tools import Data_Transformation
+from Tools import Supervised_Learning
+from Tools import Unsupervised_Learning
 import matplotlib.pyplot as plt
-import plotly.express as px
 import pandas as pd
 import numpy as np
 from datetime import *
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans
-from warnings import resetwarnings
 
 
 # https://www.kaggle.com/cdeotte/recommend-items-purchased-together-0-021/notebook
-class purchasesHistory:
+class PurchasesHistory:
     def __init__(self, df):
         self.data = df
         self.train = []
@@ -43,30 +68,8 @@ data = pd.read_csv('/Users/nakreond/Programming_Languages/Python_Projects/H&M Pe
 # change the time to sec
 data['t_dat'] = pd.to_datetime(data['t_dat'], format='%Y-%m-%d').map(pd.Timestamp.timestamp)
 
-purchasesHistory(data).lastWeekPurchases()
+purchasesHistory(data).lastWeekCustomerPurchases()
 
+# Transform data into 0 and 1
 pd.crosstab(data.customer_id, data.article_id)
 
-
-"""
-03/16 Meeting
-
-focus on:
-1. project should focus more on Event of item, like what item was bought at what day
-row is time, range can be last 30 days purchase
-col is purchase event, the feature what customer did or product
-
-2. eliminate the one time shopper and focus on more than 2 times purchase customer
-
-3. customer who have more than 2 times purchase vs who didn't purchase
-
-4. what customer do before they purchase
-
-5. use the pattern discover to find out first, than  use supervise classification to find out correct answers
-
-
-training dataset can be separated in tables:
-1. user: profile about each user 
-2. purchase with ITEM item and who buy it (time, customerID,  itemID)
-3. what item belong to what category and price	
-"""
