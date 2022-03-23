66ca03530c396c282b18d2225890f5fc7e8b4a74
ytpmeeb <85941465+ytpmeeb@users.noreply.github.com>
2022-03-23 16:58:00 -0400

    update

---
+++ Tools/main.py
@@ -0,0 +1,32 @@
+import Data_Integration
+import Data_Reduction
+import Data_Transformation
+import Anomaly_Detection
+import Supervised_Learning
+import Unsupervised_Learning
+
+
+def integration():
+    # Enter the value
+    x = [1, 1, 1, 2, 2, 1, 0, 0, 0, 0]
+    y = [0, 1, 0, 2, 2, 0, 1, 0, 0, 0]
+
+    euclidean_distance = Data_Integration.Similarity(x, y).Euclidean_distance()
+    manhattan_distance = Data_Integration.Similarity(x, y).Manhattan_distance()
+    minkowski_distance = Data_Integration.Similarity(x, y).Minkowski_distance()
+    cosine_similarity = Data_Integration.Similarity(x, y).Cosine_similarity()
+    cosine_distance = Data_Integration.Similarity(x, y).Cosine_distance()
+    pearson_correlation_coefficient = Data_Integration.Correlation(x, y).Pearson_correlation_coefficient()
+
+    print('------------Similarity------------')
+    print('Euclidean Distance between a and b is: ', euclidean_distance[0])
+    print('Manhattan Distance between a and b is: ', manhattan_distance[0])
+    print('Minkowski Distance between a and b is: ', minkowski_distance[0])
+    print('Cosine Similarity between a and b is: ', cosine_similarity[0])
+    print('Cosine Distance between a and b is: ', cosine_distance[0])
+    print('------------Correlation------------')
+    print('Pearson Correlation Coefficient between a and b is: ', pearson_correlation_coefficient[0][0])
+
+
+integration()
+
